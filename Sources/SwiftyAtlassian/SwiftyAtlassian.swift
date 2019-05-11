0503b0991fa94abb55b4cf6c7a426b29c5358c8e
Takumi Karibe <takumi.k.5610@gmail.com>
2019-05-11 10:42:15 +0900

    rename

--- Sources/SwiftyAtlassian/SwiftyAtlassian.swift
+++ Sources/SwiftyAtlassian/SwiftyAtlassian.swift
@@ -2,113 +2,92 @@ public struct User {
     public init() {}
 }
 
-protocol AtlassianProtocol {
-    associatedtype Infra
-}
-
 public protocol Infrastructure {}
 public enum Server: Infrastructure {}
 public enum Cloud: Infrastructure {}
 
-public struct Atlassian<T: Infrastructure>: AtlassianProtocol {
-    public typealias Infra = T
-}
+public struct Atlassian<Infra: Infrastructure> {}
 
 protocol ServiceProtocol {
-    associatedtype Service
     var user: User { get }
     init(withAuth user: User)
 }
 
-public protocol Method {}
-public enum Sprint: Method {}
-public enum Content: Method {}
+public protocol ProcessCategory {}
+public enum Sprint: ProcessCategory {}
+public enum Content: ProcessCategory {}
 
 public extension Atlassian {
-    struct Jira<M: Method>: ServiceProtocol {
-        public typealias Service = M
+    struct Jira<Category: ProcessCategory>: ServiceProtocol {
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct JiraSoftware<M: Method>: ServiceProtocol {
-        public typealias Service = M
+    struct JiraSoftware<Category: ProcessCategory>: ServiceProtocol {
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct JiraServiceDesk<M: Method>: ServiceProtocol {
-        public typealias Service = M
+    struct JiraServiceDesk<Category: ProcessCategory>: ServiceProtocol {
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct Bitbucket<M: Method>: ServiceProtocol {
-        public typealias Service = M
+    struct Bitbucket<Category: ProcessCategory>: ServiceProtocol {
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct Confluence<M: Method>: ServiceProtocol {
-        public typealias Service = M
+    struct Confluence<Category: ProcessCategory>: ServiceProtocol {
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
 }
 
 public extension Atlassian where Infra == Server {
-    struct Bamboo<M: Method>: ServiceProtocol {
-        public typealias Service = M
+    struct Bamboo<Category: ProcessCategory>: ServiceProtocol {
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct Crowd<M: Method>: ServiceProtocol {
-        public typealias Service = M
+    struct Crowd<Category: ProcessCategory>: ServiceProtocol {
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct FisheyeAndCrucible<M: Method>: ServiceProtocol {
-        public typealias Service = M
+    struct FisheyeAndCrucible<Category: ProcessCategory>: ServiceProtocol {
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
 }
 
 public extension Atlassian where Infra == Cloud {
-    struct Organizations<M: Method>: ServiceProtocol {
-        public typealias Service = M
+    struct Organizations<Category: ProcessCategory>: ServiceProtocol {
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct UserManagement<M: Method>: ServiceProtocol {
-        public typealias Service = M
+    struct UserManagement<Category: ProcessCategory>: ServiceProtocol {
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct UserProvisioning<M: Method>: ServiceProtocol {
-        public typealias Service = M
+    struct UserProvisioning<Category: ProcessCategory>: ServiceProtocol {
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct Trello<M: Method>: ServiceProtocol {
-        public typealias Service = M
+    struct Trello<Category: ProcessCategory>: ServiceProtocol {
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct StatusPage<M: Method>: ServiceProtocol {
-        public typealias Service = M
+    struct StatusPage<Category: ProcessCategory>: ServiceProtocol {
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct Opsgenie<M: Method>: ServiceProtocol {
-        public typealias Service = M
+    struct Opsgenie<Category: ProcessCategory>: ServiceProtocol {
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
