a3fa4bbda38e1286a3a6222260c5bf63a5ab069a
Takumi Karibe <takumi.k.5610@gmail.com>
2019-05-11 10:32:10 +0900

    rename

--- Sources/SwiftyAtlassian/SwiftyAtlassian.swift
+++ Sources/SwiftyAtlassian/SwiftyAtlassian.swift
@@ -20,95 +20,95 @@ protocol ServiceProtocol {
     init(withAuth user: User)
 }
 
-public protocol EndPoint {}
-public enum Sprint: EndPoint {}
-public enum Content: EndPoint {}
+public protocol Method {}
+public enum Sprint: Method {}
+public enum Content: Method {}
 
 public extension Atlassian {
-    struct Jira<E: EndPoint>: ServiceProtocol {
-        public typealias Service = E
+    struct Jira<M: Method>: ServiceProtocol {
+        public typealias Service = M
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct JiraSoftware<E: EndPoint>: ServiceProtocol {
-        public typealias Service = E
+    struct JiraSoftware<M: Method>: ServiceProtocol {
+        public typealias Service = M
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct JiraServiceDesk<E: EndPoint>: ServiceProtocol {
-        public typealias Service = E
+    struct JiraServiceDesk<M: Method>: ServiceProtocol {
+        public typealias Service = M
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct Bitbucket<E: EndPoint>: ServiceProtocol {
-        public typealias Service = E
+    struct Bitbucket<M: Method>: ServiceProtocol {
+        public typealias Service = M
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct Confluence<E: EndPoint>: ServiceProtocol {
-        public typealias Service = E
+    struct Confluence<M: Method>: ServiceProtocol {
+        public typealias Service = M
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
 }
 
 public extension Atlassian where Infra == Server {
-    struct Bamboo<E: EndPoint>: ServiceProtocol {
-        public typealias Service = E
+    struct Bamboo<M: Method>: ServiceProtocol {
+        public typealias Service = M
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct Crowd<E: EndPoint>: ServiceProtocol {
-        public typealias Service = E
+    struct Crowd<M: Method>: ServiceProtocol {
+        public typealias Service = M
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct FisheyeAndCrucible<E: EndPoint>: ServiceProtocol {
-        public typealias Service = E
+    struct FisheyeAndCrucible<M: Method>: ServiceProtocol {
+        public typealias Service = M
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
 }
 
 public extension Atlassian where Infra == Cloud {
-    struct Organizations<E: EndPoint>: ServiceProtocol {
-        public typealias Service = E
+    struct Organizations<M: Method>: ServiceProtocol {
+        public typealias Service = M
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct UserManagement<E: EndPoint>: ServiceProtocol {
-        public typealias Service = E
+    struct UserManagement<M: Method>: ServiceProtocol {
+        public typealias Service = M
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct UserProvisioning<E: EndPoint>: ServiceProtocol {
-        public typealias Service = E
+    struct UserProvisioning<M: Method>: ServiceProtocol {
+        public typealias Service = M
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct Trello<E: EndPoint>: ServiceProtocol {
-        public typealias Service = E
+    struct Trello<M: Method>: ServiceProtocol {
+        public typealias Service = M
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct StatusPage<E: EndPoint>: ServiceProtocol {
-        public typealias Service = E
+    struct StatusPage<M: Method>: ServiceProtocol {
+        public typealias Service = M
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
-    struct Opsgenie<E: EndPoint>: ServiceProtocol {
-        public typealias Service = E
+    struct Opsgenie<M: Method>: ServiceProtocol {
+        public typealias Service = M
         let user: User
         public init(withAuth user: User) { self.user = user }
     }
