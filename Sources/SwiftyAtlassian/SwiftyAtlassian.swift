9a748c1b505e8f0080bec6dc2f0ae03daf301db1
Takumi Karibe <takumi.k.5610@gmail.com>
2019-05-12 14:06:53 +0900

    fix access control

--- Sources/SwiftyAtlassian/SwiftyAtlassian.swift
+++ Sources/SwiftyAtlassian/SwiftyAtlassian.swift
@@ -24,76 +24,76 @@ public enum Content: ProcessCategory {}
 
 public extension Atlassian {
     struct Jira<Category: ProcessCategory>: ServiceProtocol {
-        let user: User
+        public let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
     struct JiraSoftware<Category: ProcessCategory>: ServiceProtocol {
-        let user: User
+        public let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
     struct JiraServiceDesk<Category: ProcessCategory>: ServiceProtocol {
-        let user: User
+        public let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
     struct Bitbucket<Category: ProcessCategory>: ServiceProtocol {
-        let user: User
+        public let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
     struct Confluence<Category: ProcessCategory>: ServiceProtocol {
-        let user: User
+        public let user: User
         public init(withAuth user: User) { self.user = user }
     }
 }
 
 public extension Atlassian where Infrastructure == Server {
     struct Bamboo<Category: ProcessCategory>: ServiceProtocol {
-        let user: User
+        public let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
     struct Crowd<Category: ProcessCategory>: ServiceProtocol {
-        let user: User
+        public let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
     struct FisheyeAndCrucible<Category: ProcessCategory>: ServiceProtocol {
-        let user: User
+        public let user: User
         public init(withAuth user: User) { self.user = user }
     }
 }
 
 public extension Atlassian where Infrastructure == Cloud {
     struct Organizations<Category: ProcessCategory>: ServiceProtocol {
-        let user: User
+        public let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
     struct UserManagement<Category: ProcessCategory>: ServiceProtocol {
-        let user: User
+        public let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
     struct UserProvisioning<Category: ProcessCategory>: ServiceProtocol {
-        let user: User
+        public let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
     struct Trello<Category: ProcessCategory>: ServiceProtocol {
-        let user: User
+        public let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
     struct StatusPage<Category: ProcessCategory>: ServiceProtocol {
-        let user: User
+        public let user: User
         public init(withAuth user: User) { self.user = user }
     }
     
     struct Opsgenie<Category: ProcessCategory>: ServiceProtocol {
-        let user: User
+        public let user: User
         public init(withAuth user: User) { self.user = user }
     }
 }
