608bace17efeb32baa1e0ea1f6a2c7290ba9dddc
Takumi Karibe <takumi.k.5610@gmail.com>
2019-05-12 14:24:28 +0900

    add header property

--- Sources/SwiftyAtlassian/SwiftyAtlassian.swift
+++ Sources/SwiftyAtlassian/SwiftyAtlassian.swift
@@ -1,3 +1,12 @@
+//
+//  SwiftyAtlassian.swift
+//  SwiftyAtlassian
+//
+//  Created by Takumi Karibe on 2019/05/11.
+//
+
+import Foundation
+
 public struct User {
     public let name: String
     public let password: String
@@ -13,9 +22,23 @@ public enum Cloud: InfrastructureProtocol {}
 
 public struct Atlassian<Infrastructure: InfrastructureProtocol> {}
 
-protocol ServiceProtocol {
+public protocol ServiceProtocol {
     var user: User { get }
+    var header: [String: String] { get }
     init(withAuth user: User)
+}
+
+public extension ServiceProtocol {
+    var header: [String: String] {
+        var header: [String: String] = [:]
+        header["Content-Type"] = "application/json"
+        guard let credentialData = "\(user.name):\(user.password)".data(using: String.Encoding.utf8) else {
+            fatalError("⛔️ failed to generate by user credential data.")
+        }
+        let credential = credentialData.base64EncodedString(options: [])
+        header["Authorization"] = "Basic \(credential)"
+        return header
+    }
 }
 
 public protocol ProcessCategory {}
