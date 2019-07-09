2fe05d718f653c5ea68eef5f7790511a3bf877cf
TakumiKaribe <takumi.k.5610@gmail.com>
2019-07-09 15:02:46 +0900

    add network method

---
+++ Sources/SwiftyAtlassian/Method.swift
@@ -0,0 +1,13 @@
+//
+//  Method.swift
+//  SwiftyAtlassian
+//
+//  Created by takumi-karibe on 2019/07/09.
+//
+
+import Foundation
+
+public enum Method: String {
+    case get = "GET"
+    case post = "POST"
+}
