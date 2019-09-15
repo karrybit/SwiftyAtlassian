79238d8faad5e407ddec62a56a7527fab4d25600
makanai5610 <takumi.k.5610@gmail.com>
2019-09-15 15:06:32 +0900

    fix decode container model

--- Sources/SwiftyAtlassian/Model/ResponseContainer.swift
+++ Sources/SwiftyAtlassian/Model/ResponseContainer.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 public extension Model {
-    struct ResponseContainer<T: Codable> {
-        struct Header {
+    struct ResponseContainer<T: Decodable>: Decodable {
+        struct Header: Decodable {
             public let expand: String?
             public let maxResults: Int
             public let startAt: Int
@@ -19,5 +19,11 @@ public extension Model.ResponseContainer {
     enum CodingKeys: String, CodingKey {
         case expand, maxResults, startAt, total, isLast
         case value = "values"
+    }
+    
+    init(from decoder: Decoder) throws {
+        header = try Header(from: decoder)
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        value = try container.decode(T.self, forKey: .value)
     }
 }
