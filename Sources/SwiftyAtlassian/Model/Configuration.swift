915a4e86a22514a37bf0bc1bca21d18e2bc098cd
makanai5610 <takumi.k.5610@gmail.com>
2019-09-15 16:39:30 +0900

    decode

--- Sources/SwiftyAtlassian/Model/Configuration.swift
+++ Sources/SwiftyAtlassian/Model/Configuration.swift
@@ -11,3 +11,27 @@ public extension Model {
         let ranking: Model.Configuration.Ranking
     }
 }
+
+public extension Model.Configuration {
+    enum CodingKeys: String, CodingKey {
+        case id, name, filter, estimation, ranking
+        case link = "self"
+        case columnConfig
+    }
+    
+    enum ColumnConfigKeys: String, CodingKey {
+        case column = "columns"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        link = try container.decode(URL.self, forKey: .link)
+        filter = try container.decode(Model.Configuration.Filter.self, forKey: .filter)
+        estimation = try container.decode(Model.Configuration.Estimation.self, forKey: .estimation)
+        ranking = try container.decode(Model.Configuration.Ranking.self, forKey: .ranking)
+        let nestedContainer = try container.nestedContainer(keyedBy: ColumnConfigKeys.self, forKey: .columnConfig)
+        columnConfigs = try nestedContainer.decode([Model.Configuration.ColumnConfig].self, forKey: .column)
+    }
+}
