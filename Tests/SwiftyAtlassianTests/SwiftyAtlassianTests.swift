6bf16b72250decaf8a8138c9c6f846138b91d407
makanai5610 <takumi.k.5610@gmail.com>
2019-09-19 01:44:03 +0900

    add test(failed to decode because decode to Date but to deocde type of string)

--- Tests/SwiftyAtlassianTests/SwiftyAtlassianTests.swift
+++ Tests/SwiftyAtlassianTests/SwiftyAtlassianTests.swift
@@ -2,11 +2,38 @@ import XCTest
 @testable import SwiftyAtlassian
 
 final class SwiftyAtlassianTests: XCTestCase {
-    func testExample() {
-        XCTAssertTrue(true)
+    let sprintJSON = """
+{
+    "id": 37,
+    "self": "http://www.example.com/jira/rest/agile/1.0/sprint/23",
+    "state": "future",
+    "name": "sprint 1",
+    "startDate": "2015-04-11T15:22:00.000+10:00",
+    "endDate": "2015-04-20T01:22:00.000+10:00",
+    "originBoardId": 5
+}
+"""
+    func testDecodeSprint() {
+        let data = sprintJSON.data(using: .utf8)!
+        let result: Result<Model.Sprint, Error> = API.decode(.success(data))
+        if case .failure(let error) = result {
+            debugPrint(error)
+        }
+        XCTAssertTrue(result.isSuccess())
     }
     
     static var allTests = [
-        ("testExample", testExample),
+        ("testDecodeSprint", testDecodeSprint),
     ]
+}
+
+extension Result {
+    func isSuccess() -> Bool {
+        switch self {
+        case .success(_):
+            return true
+        case .failure(_):
+            return false
+        }
+    }
 }
